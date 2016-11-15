4091cecf573405093ec31c6d687694508eb08c85
Prashant Tiwari <prashaantt@gmail.com>
2016-11-15 22:35:43 +0530

    Add save options

--- src/screens/editor/index.tsx
+++ src/screens/editor/index.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import { observer } from "mobx-react";
 import { observable, action } from "mobx";
-import { Paper, TextField, FlatButton, Dialog } from "material-ui";
+import { Paper, TextField, FlatButton, Dialog, RadioButton, RadioButtonGroup, DatePicker, TimePicker } from "material-ui";
 import { amber500 } from "material-ui/styles/colors";
 import * as ReactRouter from "react-router";
+const md = require("markdown-it")({
+    html: true,
+    linkify: true,
+    typographer: true,
+    breaks: true
+});
 
 interface PostEditorProps { }
 
@@ -20,10 +26,69 @@ interface PostEditorRefs {
     tags: TextField;
 }
 
+// type PublishOptions = "save" | "now" | "schedule";
+
+enum Publish {
+    Save,
+    Post,
+    Schedule
+}
+
 @ReactRouter.withRouter
 @observer
 export class PostEditor extends React.Component<PostEditorProps, {}> {
-    @observable private confirmDialogOpen: boolean = false;
+    @observable private confirmDialogOpen = false;
+    @observable private previewMode = false;
+    @observable private mdContent: string = "";
+    @observable private scheduled: Publish = Publish.Save;
+    private htmlContent = "";
+
+    @action private handleShowConfirmDialog = () => {
+
+        this.confirmDialogOpen = true;
+    }
+
+    @action private handleCancelCloseDialog = () => {
+
+        this.confirmDialogOpen = false;
+    }
+
+    @action private handleDiscardDraft = () => {
+
+        this.confirmDialogOpen = false;
+
+        setTimeout(() => {
+            this.injected.router.goBack();
+        }, 500);
+    }
+
+    @action togglePreview = () => {
+
+        if (this.previewMode) {
+
+            this.scheduled = Publish.Save;
+        }
+
+        this.previewMode = !this.previewMode;
+    }
+
+    @action handleMarkdownContentUpdated = (event: React.FormEvent<{}>) => {
+
+        this.mdContent = (event.target as any).value;
+    }
+
+    @action handleSchedule = (event: any, value: "save" | "now" | "schedule") => {
+
+        if (value === "save") {
+            this.scheduled = Publish.Save;
+        }
+        else if (value === "now") {
+            this.scheduled = Publish.Post;
+        }
+        else {
+            this.scheduled = Publish.Schedule;
+        }
+    }
 
     get injected() {
 
@@ -66,7 +131,7 @@ export class PostEditor extends React.Component<PostEditorProps, {}> {
 
         return {
             title: this.componentRefs.title.getValue().trim(),
-            content: this.componentRefs.content.getValue().trim(),
+            content: this.mdContent,
             excerpt: this.componentRefs.excerpt.getValue().trim(),
             seriesTitle: this.componentRefs.seriesTitle.getValue().trim(),
             subtitle: this.componentRefs.subtitle.getValue().trim(),
@@ -81,31 +146,17 @@ export class PostEditor extends React.Component<PostEditorProps, {}> {
 
     private handlePreviewClicked = () => {
 
-        console.log("preview", this.formData);
+        if (!this.previewMode) {
+
+            this.htmlContent = md.render(this.mdContent);
+        }
+
+        this.togglePreview();
     }
 
     private handleCancelClicked = () => {
 
         this.handleShowConfirmDialog();
-    }
-
-    @action private handleShowConfirmDialog = () => {
-
-        this.confirmDialogOpen = true;
-    }
-
-    @action private handleCancelCloseDialog = () => {
-
-        this.confirmDialogOpen = false;
-    }
-
-    @action private handleDiscardDraft = () => {
-
-        this.confirmDialogOpen = false;
-
-        setTimeout(() => {
-            this.injected.router.goBack();
-        }, 500);
     }
 
     private get cancelActions() {
@@ -124,6 +175,68 @@ export class PostEditor extends React.Component<PostEditorProps, {}> {
         ];
     }
 
+    private renderHtml = (): any => {
+
+        if (this.previewMode) {
+
+            return (
+                <div
+                    style={Object.assign({}, styles.textField, { marginTop: 36 })}
+                    dangerouslySetInnerHTML={{ __html: this.htmlContent }}
+                    />
+            );
+        }
+    }
+
+    private get previewModeButtons() {
+
+        return <FlatButton
+            label={Publish[this.scheduled]}
+            primary
+            onTouchTap={this.handleSaveClicked}
+            />;
+    }
+
+    private getScheduleOptions = () => {
+
+        return (
+            <div style={{ marginTop: 40 }}>
+                <div style={{ marginBottom: 20 }}>Publish options</div>
+                <RadioButtonGroup name="publish" defaultSelected="save" onChange={this.handleSchedule}>
+                    <RadioButton
+                        value="save"
+                        label="Save as draft"
+                        style={styles.radioButton}
+                        />
+                    <RadioButton
+                        value="now"
+                        label="Publish now"
+                        style={styles.radioButton}
+                        />
+                    <RadioButton
+                        value="schedule"
+                        label="Schedule for later"
+                        style={styles.radioButton}
+                        />
+                </RadioButtonGroup>
+                {this.scheduled === Publish.Schedule && this.getDateAndTimeOptions()}
+            </div>
+        );
+    }
+
+    private getDateAndTimeOptions = () => {
+
+        return (
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+                <DatePicker floatingLabelText="Pick a date" />
+                <TimePicker
+                    floatingLabelText="Choose the time"
+                    format="24hr"
+                    />
+            </div>
+        );
+    }
+
     render() {
 
         return (
@@ -134,25 +247,34 @@ export class PostEditor extends React.Component<PostEditorProps, {}> {
                         <TextField
                             floatingLabelText="Series title"
                             fullWidth
+                            disabled={this.previewMode}
                             ref={this.getSeriesRef}
+                            inputStyle={styles.textField}
                             />
                         <TextField
                             floatingLabelText="Post title"
                             floatingLabelStyle={{ color: amber500 }}
                             fullWidth
+                            disabled={this.previewMode}
                             ref={this.getTitleRef}
+                            inputStyle={styles.textField}
                             />
                         <TextField
                             floatingLabelText="Subtitle"
                             fullWidth
+                            disabled={this.previewMode}
                             ref={this.getSubtitleRef}
+                            inputStyle={styles.textField}
                             />
                         <TextField
                             floatingLabelText="Excerpt"
                             rows={4}
                             multiLine
                             fullWidth
+                            disabled={this.previewMode}
                             ref={this.getExcerptRef}
+                            textareaStyle={styles.textField}
+                            inputStyle={styles.textField}
                             />
                         <TextField
                             floatingLabelText="Content"
@@ -160,24 +282,27 @@ export class PostEditor extends React.Component<PostEditorProps, {}> {
                             fullWidth
                             rows={20}
                             multiLine
+                            disabled={this.previewMode}
                             ref={this.getContentRef}
+                            onChange={this.handleMarkdownContentUpdated}
+                            value={!this.previewMode ? this.mdContent : ""}
+                            children={this.renderHtml()}
                             />
                         <TextField
                             floatingLabelText="Tags"
                             fullWidth
                             ref={this.getTagsRef}
+                            disabled={this.previewMode}
+                            inputStyle={styles.textField}
                             />
+                        {this.previewMode && this.getScheduleOptions()}
                         <div style={styles.buttonSeparator}>
                             <FlatButton
-                                label="Save"
-                                primary
-                                onTouchTap={this.handleSaveClicked}
-                                />
-                            <FlatButton
-                                label="Preview"
+                                label={!this.previewMode ? "Preview" : "Edit"}
                                 primary
                                 onTouchTap={this.handlePreviewClicked}
                                 />
+                            {this.previewMode && this.previewModeButtons}
                             <FlatButton
                                 label="Cancel"
                                 secondary
@@ -210,5 +335,11 @@ const styles = {
     },
     buttonSeparator: {
         marginTop: 32
-    } as React.CSSProperties
+    } as React.CSSProperties,
+    textField: {
+        color: "inherit"
+    },
+    radioButton: {
+        marginBottom: 16,
+    }
 };
